f01e73c300b329a9cc998c9bd448926d415f63d6
Timur Zakirov <mr.tiebow@mail.ru>
2025-02-19 22:13:30 +0300

    developing

---
+++ src/assets/js/main.ts
@@ -0,0 +1,209 @@
+import KeenSlider from 'keen-slider'
+
+function mainSliderNavigation(slider) {
+    let wrapper, dots, arrowLeft, arrowRight;
+
+    function markup(remove?: boolean) {
+        wrapperMarkup(remove);
+        dotMarkup(remove);
+        arrowMarkup(remove);
+    }
+
+    function removeElement(elment: HTMLElement) {
+        elment.parentNode.removeChild(elment);
+    }
+    function createDiv(className: string) {
+        let div = document.createElement("div");
+        let classNames = className.split(" ");
+        classNames.forEach((name) => div.classList.add(name));
+        return div;
+    }
+
+    function arrowMarkup(remove?: boolean) {
+        if (remove) {
+            removeElement(arrowLeft);
+            removeElement(arrowRight);
+            return;
+        }
+        arrowLeft = createDiv("arrow arrow--left");
+        arrowLeft.addEventListener("click", () => slider.prev());
+        arrowRight = createDiv("arrow arrow--right");
+        arrowRight.addEventListener("click", () => slider.next());
+
+        wrapper.appendChild(arrowLeft);
+        wrapper.appendChild(arrowRight);
+    }
+
+    function wrapperMarkup(remove?: boolean) {
+        if (remove) {
+            let parent = wrapper.parentNode;
+            while (wrapper.firstChild)
+                parent.insertBefore(wrapper.firstChild, wrapper);
+            removeElement(wrapper);
+            return;
+        }
+        wrapper = createDiv("mainSliderNavigation-wrapper");
+        slider.container.parentNode.appendChild(wrapper);
+        wrapper.appendChild(slider.container);
+    }
+
+    function dotMarkup(remove) {
+        if (remove) {
+            removeElement(dots);
+            return;
+        }
+        dots = createDiv("dots");
+        slider.track.details.slides.slice(0, slider.track.details.slides.length - 1).forEach((_, idx: number) => {
+            let dot = createDiv("dot");
+            dot.addEventListener("click", () => slider.moveToIdx(idx));
+            dots.appendChild(dot);
+        });
+        wrapper.appendChild(dots);
+    }
+
+    function updateClasses() {
+        let slide = slider.track.details.rel;
+        slide === 0
+            ? arrowLeft.classList.add("arrow--disabled")
+            : arrowLeft.classList.remove("arrow--disabled");
+        slide === slider.track.details.slides.length - 1
+            ? arrowRight.classList.add("arrow--disabled")
+            : arrowRight.classList.remove("arrow--disabled");
+        Array.from(dots.children).forEach(function (dot: HTMLElement, idx: number) {
+            idx === slide
+                ? dot.classList.add("dot--active")
+                : dot.classList.remove("dot--active");
+        });
+    }
+
+    slider.on("created", () => {
+        markup();
+        updateClasses();
+    });
+    slider.on("optionsChanged", () => {
+        markup(true);
+        markup();
+        updateClasses();
+    });
+    slider.on("slideChanged", () => {
+        updateClasses();
+    });
+    slider.on("destroyed", () => {
+        markup(true);
+    });
+}
+
+function productsSliderNavigation(slider) {
+    let wrapper, dots, arrowLeft, arrowRight;
+
+    function markup(remove?: boolean) {
+        wrapperMarkup(remove);
+        dotMarkup(remove);
+        arrowMarkup(remove);
+    }
+
+    function removeElement(elment: HTMLElement) {
+        elment.parentNode.removeChild(elment);
+    }
+    function createDiv(className: string) {
+        let div = document.createElement("div");
+        let classNames = className.split(" ");
+        classNames.forEach((name) => div.classList.add(name));
+        return div;
+    }
+
+    function arrowMarkup(remove?: boolean) {
+        if (remove) {
+            removeElement(arrowLeft);
+            removeElement(arrowRight);
+            return;
+        }
+        arrowLeft = createDiv("arrow arrow--left");
+        arrowLeft.addEventListener("click", () => slider.prev());
+        arrowRight = createDiv("arrow arrow--right");
+        arrowRight.addEventListener("click", () => slider.next());
+
+        wrapper.appendChild(arrowLeft);
+        wrapper.appendChild(arrowRight);
+    }
+
+    function wrapperMarkup(remove?: boolean) {
+        if (remove) {
+            let parent = wrapper.parentNode;
+            while (wrapper.firstChild)
+                parent.insertBefore(wrapper.firstChild, wrapper);
+            removeElement(wrapper);
+            return;
+        }
+        wrapper = createDiv("products-slider-wrapper");
+        slider.container.parentNode.appendChild(wrapper);
+        wrapper.appendChild(slider.container);
+    }
+
+    function dotMarkup(remove) {
+        if (remove) {
+            removeElement(dots);
+            return;
+        }
+        dots = createDiv("dots");
+        slider.track.details.slides.slice(0, slider.track.details.slides.length - 1).forEach((_, idx: number) => {
+            let dot = createDiv("dot");
+            dot.addEventListener("click", () => slider.moveToIdx(idx));
+            dots.appendChild(dot);
+        });
+        wrapper.appendChild(dots);
+    }
+
+    function updateClasses() {
+        let slide = slider.track.details.rel;
+        slide === 0
+            ? arrowLeft.classList.add("arrow--disabled")
+            : arrowLeft.classList.remove("arrow--disabled");
+        slide === slider.track.details.slides.length - 1
+            ? arrowRight.classList.add("arrow--disabled")
+            : arrowRight.classList.remove("arrow--disabled");
+        Array.from(dots.children).forEach(function (dot: HTMLElement, idx: number) {
+            idx === slide
+                ? dot.classList.add("dot--active")
+                : dot.classList.remove("dot--active");
+        });
+    }
+
+    slider.on("created", () => {
+        markup();
+        updateClasses();
+    });
+    slider.on("optionsChanged", () => {
+        markup(true);
+        markup();
+        updateClasses();
+    });
+    slider.on("slideChanged", () => {
+        updateClasses();
+    });
+    slider.on("destroyed", () => {
+        markup(true);
+    });
+}
+
+new KeenSlider(
+    "#main-slider",
+    {
+        slides: {
+            perView: 2,
+            spacing: 10,
+        },
+    },
+    [mainSliderNavigation]
+);
+
+new KeenSlider(
+    ".products-slider",
+    {
+        slides: {
+            perView: 5,
+            spacing: 54,
+        },
+    },
+    [productsSliderNavigation]
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,82 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 export default {
     content: [
         './src/**/*.{js,html,njk}'
-    ]
+    ],
+    theme: {
+        screens: {
+            m320: '320px',
+            m375: '375px',
+            m475: '475px',
+            m576: '576px',
+            m768: '768px',
+            m1024: '1024px',
+            m1280: '1280px',
+        },
+        fontFamily: {
+            sans: ['LVC Sans', ...fontFamily.sans],
+        },
+        extend: {
+            colors: {
+                sale: '#DD4242',
+                main: '#6B6862',
+                bg: '#F6F6F6',
+                accent: '#5AB77F',
+                secondary: '#A7A7A7',
+                border: '#E7E1E1'
+            }
+        }
+    },
+    corePlugins: {
+        container: false,
+    },
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
+    plugins: [
+        ({ addUtilities, theme, addVariant }) => {
+            const spacing = theme('width');
+
+            const sizeUtility = Object.entries(spacing).reduce(
+                (acc, [key, value]) => {
+                    acc[`.min-size-${key.replace(/[./]/g, '\\$&')}`] = {
+                        'min-width': value,
+                        'min-height': value,
+                    };
+                    acc[`.max-size-${key.replace(/[./]/g, '\\$&')}`] = {
+                        'max-width': value,
+                        'max-height': value,
+                    };
+                    return acc;
+                },
+                {},
+            );
+
+            addUtilities({
+                ...sizeUtility,
+                '.flex-center': {
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                },
+                '.pos-abs': {
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                },
+                '.pos-abs-x': {
+                    position: 'absolute',
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                },
+                '.pos-abs-y': {
+                    position: 'absolute',
+                    top: '50%',
+                    transform: 'translateY(-50%)',
+                },
+            });
+        },
+    ],
 }
