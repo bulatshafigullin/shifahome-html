24f4075c624dc9c08726c33c4adc0b9c4a390ff6
bulatsh <bulatsh]>
2025-03-14 16:26:44 +0300

    setup building for prod

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,27 @@ import nunjucks from '@vituum/vite-plugin-nunjucks'
 import tailwindcss from '@vituum/vite-plugin-tailwindcss'
 
 export default {
+    build: {
+        manifest: true,
+        modulePreload: false,
+            rollupOptions: { 
+                output: {
+                    entryFileNames: "static/cos/[name]-[hash].js",
+                    assetFileNames: (assetInfo) => {
+                        console.log('assetInfo', assetInfo)
+                        if (assetInfo.originalFileName) {
+                            if (assetInfo.originalFileName.endsWith(".ts")) {
+                                console.log(assetInfo)
+                            }
+                            let path = assetInfo.originalFileName.replace("src/assets/", "static/cos/").replace("src/styles", "static/cos/styles").replace(assetInfo.name, "[name]-[hash].[ext]");
+                            return path
+                        }
+                        
+                        return "static/cos/[name]-[hash].[ext]"
+                    }
+                }
+            }
+        },
     plugins: [vituum(), nunjucks({
         root: './src'
     }),
